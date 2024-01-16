24e533deb77841f1457ee729f0d5d321a0b43746
RajaAmarSiregar <98572394+RajaAmarSiregar@users.noreply.github.com>
2024-01-16 17:47:23 +0700

    button

--- js/ok.js
+++ js/ok.js
@@ -1,3 +1,19 @@
+function tampilkanJudulLuasPersegi() {
+    var judulLuasPersegi = document.querySelector('.judul_luas_persegi');
+    var judulLuasPanjang = document.querySelector('.judul_luas_panjang');
+    
+    judulLuasPersegi.style.display = 'block';
+    judulLuasPanjang.style.display = 'none';
+}
+
+function tampilkanJudulLuasPanjang() {
+    var judulLuasPersegi = document.querySelector('.judul_luas_persegi');
+    var judulLuasPanjang = document.querySelector('.judul_luas_panjang');
+    
+    judulLuasPersegi.style.display = 'none';
+    judulLuasPanjang.style.display = 'block';
+}
+
 function luas() {
     var s = document.getElementById('s').value;
 
